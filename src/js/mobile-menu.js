304c4d6efaaaa0d162bf09d1a05daabd67bd169c
demonsys <112822206+demonsys@users.noreply.github.com>
2023-03-24 17:32:46 +0200

    2

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -8,18 +8,17 @@
     mobileMenuRef.classList.toggle('menu-container--open');
     menuBtnRef.classList.toggle('menu__button--open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
-
     const scrollLockMethod = !expanded
       ? 'disableBodyScroll'
       : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
 
     // Close the mobile menu on wider screens if the device orientation changes
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-      if (!e.matches) return;
-      mobileMenuRef.classList.remove('menu-container--open');
-      menuBtnRef.setAttribute('aria-expanded', false);
-      bodyScrollLock.enableBodyScroll(document.body);
-    });
+    // window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    //   if (!e.matches) return;
+    //   mobileMenuRef.classList.remove('menu-container--open');
+    //   menuBtnRef.setAttribute('aria-expanded', false);
+    //   bodyScrollLock.enableBodyScroll(document.body);
+    // });
   });
 })();
