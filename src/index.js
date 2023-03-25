c96587d9720dcb2a089e7dc3154277039c6f64f1
Your Name <pavlo.morozenko@ukr.net>
2023-03-25 21:28:17 +0100

    firstDraft

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import * as newsCard from './js/newsCard';
 import FETCHNEWS from './js/fetchNews';
-import { themeSwitch, iconSun, iconMoon, labelEl, bodyEl, pointerLight, pointerDark, borderEl} from "./js/theme-switcher";
+import {
+  themeSwitch,
+  iconSun,
+  iconMoon,
+  labelEl,
+  bodyEl,
+  pointerLight,
+  pointerDark,
+  borderEl,
+} from './js/theme-switcher';
+import * as date from './js/current-date';
---
+++ src/js/current-date.js
@@ -0,0 +1,21 @@
+/* function of zero adding */
+/* (if number is less then 10, adding zero in front of it) */
+function zero_first_format(value) {
+  if (value < 10) {
+    value = '0' + value;
+  }
+  return value;
+}
+
+/* function of receiving of current date */
+function date() {
+  var current_date = new Date();
+  var day = zero_first_format(current_date.getDate());
+  var month = zero_first_format(current_date.getMonth() + 1);
+  var year = current_date.getFullYear();
+
+  return day + '/' + month + '/' + year;
+}
+
+/* show current date with id "current_date" */
+document.getElementById('current_date').innerHTML = date();
