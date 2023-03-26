2c89a520e09309b18b6996b42543974985664781
Your Name <pavlo.morozenko@ukr.net>
2023-03-26 22:15:18 +0200

    draft 3

--- src/js/current-date.js
+++ src/js/current-date.js
@@ -1,23 +1,39 @@
-/* function of zero adding */
-/* (if number is less then 10, adding zero in front of it) */
-function zero_first_format(value) {
-  if (value < 10) {
-    value = '0' + value;
-  }
-  return value;
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+// import { filterByDateMostViwed } from './fetchData/filters';
+
+const datePicker = document.querySelector('#date-picker');
+const dateInput = document.querySelector('.date-input');
+
+const dateUp = document.querySelector('.date-up');
+const dateDown = document.querySelector('.date-down');
+const calendarSvg = document.querySelector('.date-down');
+
+let selectedDate;
+
+const options = {
+  //allowInput: true,
+  dateFormat: 'd/m/Y',
+  locale: 'en',
+  onChange: function (selectedDates) {
+    selectedDate = selectedDates[0];
+
+    datePicker.style.backgroundColor = '#4440F6';
+    datePicker.style.color = '#F8F8F8';
+    datePicker.style.border = '#4440F6';
+    dateDown.style.display = 'block';
+    dateUp.style.display = 'none';
+  },
+};
+flatpickr(datePicker, options);
+
+dateInput.addEventListener('click', changeSvg);
+
+function changeSvg(e) {
+  dateInput.style.color = '#F8F8F8';
+  dateDown.style.display = 'none';
+  dateUp.style.display = 'block';
 }
-
-/* function of receiving of current date */
-function date() {
-  var current_date = new Date();
-  var day = zero_first_format(current_date.getDate());
-  var month = zero_first_format(current_date.getMonth() + 1);
-  var year = current_date.getFullYear();
-
-  return day + '/' + month + '/' + year;
+function clearFlatpickr() {
+  datePicker.clear();
 }
-
-/* show current date with id "current_date" */
-document.getElementById('current_date').innerHTML = date();
-
-export {date, zero_first_format};
--- src/js/fetchNews.js
+++ src/js/fetchNews.js
@@ -4,19 +4,18 @@ import axios from 'axios';
 const appID = 'IIf4vmmTbfNyDLUoXuoPyrjTHkEJuSUj';
 
 async function fetchNews(categ) {
+  fetch(`https://api.nytimes.com/svc/mostpopular/v2${categ}?api-key=${appID}`);
 
-  fetch(`https://api.nytimes.com/svc/mostpopular/v2${categ}?api-key=${appID}`)
-    
-    try {
-        const response = await axios.get(`https://api.nytimes.com/svc/mostpopular/v2${categ}?api-key=${appID}`)
-        response.data.results.map(element => {
-            RENDERCARD.renderCard(element);
-        })
-        
-    }
-    catch (error) { 
-        console.log(error)
-    }
+  try {
+    const response = await axios.get(
+      `https://api.nytimes.com/svc/mostpopular/v2${categ}?api-key=${appID}`
+    );
+    response.data.results.map(element => {
+      RENDERCARD.renderCard(element);
+    });
+  } catch (error) {
+    console.log(error);
   }
+}
 
 export default { fetchNews };
