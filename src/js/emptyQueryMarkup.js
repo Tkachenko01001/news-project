6259d4e45f50bf809ad5452b5f7fc92f8714aba2
Andrii Doroshenko <doroshenkoandrey@ukr.net>
2023-03-26 10:45:53 +0300

    feature_empty_query

---
+++ src/js/emptyQueryMarkup.js
@@ -0,0 +1,3 @@
+function emptyQueryMarkup(data) {
+    const template = 
+}
--- src/js/fetchNews.js
+++ src/js/fetchNews.js
@@ -3,12 +3,14 @@ import axios from 'axios';
 const appID = 'IIf4vmmTbfNyDLUoXuoPyrjTHkEJuSUj';
 
 async function fetchNews(categ) {
-  fetch(`https://api.nytimes.com/svc/mostpopular/v2${categ}?api-key=${appID}`);
-
   try {
     const response = await axios.get(
       `https://api.nytimes.com/svc/mostpopular/v2${categ}?api-key=${appID}`
     );
+
+    /* if (response.data.num_results === 0) {
+    } */
+
     response.data.results.map(element => {
       RENDERCARD.renderCard(element);
     });
