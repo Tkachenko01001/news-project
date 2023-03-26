e0258f29379f885526f29a19a9cb133aed77710f
demonsys <112822206+demonsys@users.noreply.github.com>
2023-03-26 17:39:51 +0300

    1

--- src/js/get-current-page.js
+++ src/js/get-current-page.js
@@ -1,21 +1,17 @@
 const menuRef = document.querySelector('.nav-menu');
-if (location.pathname === '/read.html') {
-  menuRef.lastElementChild.firstElementChild.classList.add(
-    'nav-menu__link--active'
-  );
-}
-switch (location.pathname) {
-  case '/news-project/index.html':
+console.log('Current page is', location.pathname.match(/[^/]+\.html/i)[0]);
+switch (location.pathname.match(/[^/]+\.html/i)[0]) {
+  case 'index.html':
     menuRef.children[0].firstElementChild.classList.add(
       'nav-menu__link--active'
     );
     break;
-  case '/news-project/favorite.html':
+  case 'favorite.html':
     menuRef.children[1].firstElementChild.classList.add(
       'nav-menu__link--active'
     );
     break;
-  case '/news-project/read.html':
+  case 'read.html':
     menuRef.children[2].firstElementChild.classList.add(
       'nav-menu__link--active'
     );
