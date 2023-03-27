c3f3d3ef243c874e0472229a276f1e0459713049
demonsys <112822206+demonsys@users.noreply.github.com>
2023-03-28 01:35:04 +0300

    Сделал фильтр по дате

--- src/js/sortCardsByDate.js
+++
@@ -1,8 +0,0 @@
-const galleryNewsRefs = document.querySelector('.galleryNews');
-const cardDate = galleryNewsRefs.querySelectorAll('div');
-console.log(galleryNewsRefs.children);
-// async function currentCards() {
-//   // ...
-// }
-const card1 = galleryNewsRefs.children[1];
-console.log(card1);
