ee9ca39fe8198770175d65035702582ebfa6ce2f
Your Name <pavlo.morozenko@ukr.net>
2023-03-27 13:41:10 +0200

    draft5

--- src/js/fetchNews.js
+++ src/js/fetchNews.js
@@ -4,19 +4,17 @@ import axios from 'axios';
 const appID = 'IIf4vmmTbfNyDLUoXuoPyrjTHkEJuSUj';
 
 async function fetchNews(categ) {
-    
-    try {
-        const response = await axios.get(`https://api.nytimes.com${categ}?api-key=${appID}`)
-        response.data.results
-          .map(element => {
-            RENDERCARD.renderCard(element);
-          })
-          .join('');
-        
-    }
-    catch (error) { 
-        console.log(error)
-    }
+  try {
+    const response = await axios.get(
+      `https://api.nytimes.com${categ}?api-key=${appID}`
+    );
+    response.data.results
+      .map(element => {
+        RENDERCARD.renderCard(element);
+      })
+      .join('');
+  } catch (error) {
+    console.log(error);
   }
 }
 
