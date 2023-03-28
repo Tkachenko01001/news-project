1da22a5c77845dddfc7029108c71d05c5c1117b4
Pavlo-Savka <savka.pavlo@gmail.com>
2023-03-28 16:56:53 +0300

    Функція пошуку по категоріях

--- src/js/newsCard.js
+++ src/js/newsCard.js
@@ -5,18 +5,3 @@ const galleryCategories = document.querySelector('.button-rend');
 const category = '/svc/mostpopular/v2/viewed/7.json';
 
 FETCHNEWS.fetchNews(category);
-
-// Функція пошуку по категоріях готова!!!
-//Розкоментувати коли буде блок.button - rend
-// та кнопки категорій .button
-
-// galleryCategories.addEventListener('click', e => {
-//   console.log(e.target);
-//   if (e.target.closest('.button')) {
-//     galleryNews.innerHTML = '';
-//     FETCHNEWSBYCATEGORY.fetchNews(e.target.textContent);
-//   }
-// });
-
-// - тестовий запуск по категорії бізнес:
-//FETCHNEWSBYCATEGORY.fetchNews('business');
