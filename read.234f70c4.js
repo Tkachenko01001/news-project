daffe20bdf38da65695697ee2070522fd5163c8a
Tkachenko01001 <113258288+Tkachenko01001@users.noreply.github.com>
2023-03-28 19:01:21 +0000

    Deploying to gh-pages from @ Tkachenko01001/news-project@623cb891c378d23b91b0483dca1fae30540faf07 🚀

--- read.234f70c4.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".search-bar"),c=document.querySelector(".search-icon");document.querySelector(".close-icon");c.addEventListener("click",(function(){e.classList.toggle("open")}))}();
-//# sourceMappingURL=read.234f70c4.js.map
---
+++ read.235deaf5.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".search-bar"),c=document.querySelector(".search-icon-mob");document.querySelector(".close-icon");c.addEventListener("click",(function(){e.classList.toggle("open")}))}();
+//# sourceMappingURL=read.235deaf5.js.map
