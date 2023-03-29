98c8e285c4bd2f81a98cc522418e645ee26ff414
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-03-29 12:06:41 +0300

    fixes bugs

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,18 +1,16 @@
 import axios from 'axios';
-import RENDERCARD from './renderCard';
-import FETCHNEWSBYCATEGORY from './fetchNewsByCategory';
+import { NYTNewsAPI } from './fetchNews';
+import { format } from 'date-fns';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import RENDERCARD from './renderCard';
+// import FETCHNEWSBYCATEGORY from './fetchNewsByCategory';
 import '../weather-card-markup';
 import { NYTNewsAPI } from './fetchNews';
 import galleryMarkup from './gallery-markup';
 
 const galleryNews = document.querySelector('.galleryNews');
-
-// const pgContainer = document.querySelector('.pagination-container');
-// const pg = document.getElementById('pagination');
-// const btnNextPg = document.querySelector('.btn-list_next-page');
-// const btnPrevPg = document.querySelector('.btn-list_prev-page');
-// const btnFirstPg = document.querySelector('.btn-list_first-page');
-// const btnLastPg = document.querySelector('.btn-list_last-page');
+const buttonContainer = document.querySelector('.button-container');
+const buttonsInModal = document.querySelector('#modalContent');
 
 const pg = document.getElementById('pagination');
 const btnNextPg = document.querySelector('button.next-page');
@@ -26,20 +24,6 @@ const valuePage = {
 };
 
 pagination();
-
-// pg.addEventListener('click', e => {
-//   const ele = e.target;
-
-//   if (ele.dataset.page) {
-//     const pageNumber = parseInt(e.target.dataset.page, 10);
-
-//     valuePage.curPage = pageNumber;
-//     pagination(valuePage);
-
-//     handleButtonLeft();
-//     handleButtonRight();
-//   }
-// });
 
 function pagination() {
   const { totalPages, curPage, numLinksTwoSide: delta } = valuePage;
@@ -175,7 +159,7 @@ if (window.innerWidth < 768) {
 
 pg.addEventListener('click', renderPageOnButtonClick);
 
-function renderPageOnButtonClick(e) {
+function renderPageOnButtonClick(e) {  
   if (e.target.nodeName !== 'LI') {
     return;
   }
@@ -194,3 +178,151 @@ function renderPageOnButtonClick(e) {
 }
 
 renderCard(perPage, 1);
+// Пошук за пошуковим запитом:
+const formSearch = document.querySelector('.search-form');
+const searchBar = document.querySelector('.search-bar form');
+formSearch.addEventListener('submit', handleSubmit);
+searchBar.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  if (e.target.style.display === 'none') {
+    return;
+  }
+  galleryNews.innerHTML = '';
+  renderSearchQueryCard(e.target.querySelector('input').value, '');
+}
+
+async function renderSearchQueryCard(query, filter) {
+  try {
+    const data = await NYTNewsAPI.getNewsBySearchQuery(query, filter);
+    if (!data.response.docs) {
+      Notify.warning("error");
+      return;
+    } else {
+      const finalResult = data.response.docs
+        .map(e => {
+          const img = [e][0]?.multimedia?.[0]?.url;
+          const mediaUrl = img
+            ? `https://static01.nyt.com/${img}`
+            : 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/390px-No-Image-Placeholder.svg.png';
+          const mediaAlt = [e][0]?.multimedia?.[0]?.crop_name;
+          const newsCategory = [e][0].section_name;
+          const title = [e][0].headline?.main;
+          const subscribe = [e][0].abstract;
+          const date = format(Date.parse([e][0].pub_date), 'yyyy-MM-dd');
+          const url = [e][0].web_url;
+          const ID = [e][0].uri;
+          const cardFavText = 'Add to favorites';
+          return `
+          <div class="card" id="${ID}">
+            <div class="card__img-wrapper">
+              <img class="card__img" src="${mediaUrl}" alt="${mediaAlt}">
+              <span class="card__category">${newsCategory}</span>
+              <div class="card__favorite">
+                <span class="card__favText">${cardFavText}</span>
+                <button class="card__favBtn" type="button" >
+                  <svg class="card__favIcon" width="16" height="16" viewBox="0 0 32 32">
+                    <path class="card__heart" stroke="#4440F7" style="stroke: var(--color3, #4440F7)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2" d="M9.333 4c-3.681 0-6.667 2.955-6.667 6.6 0 2.943 1.167 9.927 12.651 16.987 0.206 0.125 0.442 0.191 0.683 0.191s0.477-0.066 0.683-0.191c11.484-7.060 12.651-14.044 12.651-16.987 0-3.645-2.985-6.6-6.667-6.6s-6.667 4-6.667 4-2.985-4-6.667-4z"></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <h2 class="card__title">${title}</h2>
+            <p class="card__subscribe">${subscribe}</p>
+            <span class="card__date">${date}</span>
+            <a href="${url}" target="_blank">
+              <span class="card__read-more">Read more</span>
+            </a>
+          </div>
+        `;
+        })
+        .join('');
+      galleryNews.insertAdjacentHTML('beforeend', finalResult);
+    }
+  } catch (error) {
+      Notify.warning(error);
+    }
+  }
+
+
+// Пошук за категорією:
+
+buttonContainer.addEventListener('click', e => {
+  if (e.target.closest('.button-ctg')) {
+    galleryNews.innerHTML = '';
+    renderSearchByCategoryCard(e.target.textContent.toLowerCase());
+  }
+});
+const modal = document.getElementById('modal');
+buttonsInModal.addEventListener('click', e => {
+  if (e.target.closest('.more-item-categories')) {
+    modal.style.display = 'none';
+    galleryNews.innerHTML = '';
+    renderSearchByCategoryCard(encodeURI(e.target.textContent.toLowerCase()));
+  }
+});
+
+async function renderSearchByCategoryCard(categ) {
+      const data = await NYTNewsAPI.getNewsByCategories(categ);
+     if (!data.results) {
+        Notify.warning("error");
+       return
+     } else {
+       try {
+       const finalResult = data.results
+         .map(e => {
+           const img = [e][0]?.multimedia?.[2]?.url;
+           const mediaUrl = img
+             ? img
+             : 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/390px-No-Image-Placeholder.svg.png';
+           const mediaAlt = [e][0]?.multimedia?.[2]?.caption;
+           const newsCategory = [e][0]?.section;
+           const title = [e][0]?.title;
+           const subscribe = [e][0]?.abstract;
+           const date = format(
+             Date.parse([e][0]?.published_date),
+             'yyyy-MM-dd'
+           );
+           const url = [e][0].url;
+           const ID = [e][0].uri;
+           const cardFavText = 'Add to favorites';
+          return `
+          <div class="card" id="${ID}">
+            <div class="card__img-wrapper">
+              <img class="card__img" src="${mediaUrl}" alt="${mediaAlt}">
+              <span class="card__category">${newsCategory}</span>
+              <div class="card__favorite">
+                <span class="card__favText">${cardFavText}</span>
+                <button class="card__favBtn" type="button" >
+                  <svg class="card__favIcon" width="16" height="16" viewBox="0 0 32 32">
+                    <path class="card__heart" stroke="#4440F7" style="stroke: var(--color3, #4440F7)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2" d="M9.333 4c-3.681 0-6.667 2.955-6.667 6.6 0 2.943 1.167 9.927 12.651 16.987 0.206 0.125 0.442 0.191 0.683 0.191s0.477-0.066 0.683-0.191c11.484-7.060 12.651-14.044 12.651-16.987 0-3.645-2.985-6.6-6.667-6.6s-6.667 4-6.667 4-2.985-4-6.667-4z"></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <h2 class="card__title">${title}</h2>
+            <p class="card__subscribe">${subscribe}</p>
+            <span class="card__date">${date}</span>
+            <a href="${url}" target="_blank">
+              <span class="card__read-more">Read more</span>
+            </a>
+          </div>
+        `
+         })
+           .join('');
+         galleryNews.insertAdjacentHTML('beforeend', finalResult);
+       } catch (error) {
+         Notify.warning(error);
+       }
+     }
+}
+
+function ifEmptyQuery() {
+  const emptyQuerySection = document.querySelector('.js-empty-query');
+  if (galleryNews.children.length === 0) {
+    emptyQuerySection.classList.remove('empty-query');
+  } else {
+    emptyQuerySection.classList.add('empty-query');
+  }
+}
