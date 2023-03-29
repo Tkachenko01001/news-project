6d8e2ca07e1cfd88ba86090985151c64c1565cf2
Tkachenko01001 <113258288+Tkachenko01001@users.noreply.github.com>
2023-03-29 14:47:26 +0000

    Deploying to gh-pages from @ Tkachenko01001/news-project@5029630b41a1ff23642078f1ea2d511a995cc54d 🚀

---
+++ read.0586769c.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".accordion-header").forEach((function(e){e.addEventListener("click",(function(){e.nextElementSibling.classList.toggle("accordion-header-open-content")}))}));
+//# sourceMappingURL=read.0586769c.js.map
